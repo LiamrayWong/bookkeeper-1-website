c2ad15a9de9f9002b98d0fe0878d84d44f0e9f14
Ryan <1079388320@qq.com>
2020-08-15 12:03:10 +0800

    deploy

---
+++ precache-manifest.9e568a6ac68fa334cac8d05cc6c00aea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324aed878a151e5a1272003cf49bb7af",
+    "url": "/bookkeeper-1-website/index.html"
+  },
+  {
+    "revision": "8cc1b6e4996015349ef2",
+    "url": "/bookkeeper-1-website/static/css/main.59a1bd8c.chunk.css"
+  },
+  {
+    "revision": "baa49b17c30ae632b5d8",
+    "url": "/bookkeeper-1-website/static/js/2.cfb4b621.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/bookkeeper-1-website/static/js/2.cfb4b621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cc1b6e4996015349ef2",
+    "url": "/bookkeeper-1-website/static/js/main.f0bac8b7.chunk.js"
+  },
+  {
+    "revision": "04af0d9beecedf1684bf",
+    "url": "/bookkeeper-1-website/static/js/runtime-main.9ba9183d.js"
+  }
+]);
